7b1286a88391cfafd13e1e1c3b62635156052dcd
Chinmay Shah <56558586+cjshah032@users.noreply.github.com>
2020-09-19 12:55:00 +0530

    Correct mistakes in Top Sort

--- Snippets/Graph/Topological Sorting (single order).cpp
+++ Snippets/Graph/Topological Sorting (single order).cpp
@@ -2,7 +2,7 @@
 //visited = boolean array 
 //v = no. of vertices
 //ts = vector to store topsort order. Should be read backwards.
-void dfs_mod (int dnode, set<int> *graph, bool *visited, vector<char> ts){ //mod to imply modified dfs
+void dfs_mod (int dnode, set<int> graph[], bool visited[], vi &ts){ //mod to imply modified dfs
     visited[dnode] = true;
     for(auto itr = graph[dnode].begin(); itr != graph[dnode].end(); ++itr)
     {
@@ -10,22 +10,24 @@ void dfs_mod (int dnode, set<int> *graph, bool *visited, vector<char> ts){ //mod
         continue;
         else{
             visited[*itr] = true;
-            dfs(*itr, graph, visited);
-            ts.push_back((char)(*itr + 'A'));
+            dfs_mod(*itr, graph, visited, ts);
         }
     }
+    ts.push_back(dnode);
 }
 
+
+//to be used in main:
 for(int l = 0; l < v; l++)
 {
     if(visited[l] == true)
         continue;
         
     else{
-        dfs(l, graph, visited);
+        dfs_mod(l, graph, visited, ts);
     }
 }
 
 //Displaying topsort order:
 for(auto itr = ts.rbegin(); itr != ts.rend(); ++itr)
-    cout<<*itr<<endl;
+    cout<<*itr;
