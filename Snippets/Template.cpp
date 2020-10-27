8a4822a70f646767605f4a944234a6de1d0f6f3f
Chinmay Shah <cjshah2000@gmail.com>
2020-10-27 19:57:28 +0530

    Added till template

---
+++ Snippets/Template.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+//input//
+#define FAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define NArray int n; cin>>n; int a[n]; inputN
+#define inputNxN for(int x=0; x<n; x++)for(int y=0; y<n; y++)cin>>a[x][y];
+#define inputN for(int z=0; z<n; z++)cin>>a[z];
+#define endl '\n'
+
+//variable declarations//
+#define vi  vector<int>
+#define vii vector<pair<int, int>>
+#define vs  vector<string>
+#define qi queue<int>
+#define long_max 9223372036854775807
+#define int_max 2147483647
+
+//common functions//
+#define ll long long
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define bitcount _builtin_popcount
+#define leadzero __builtin_clz
+#define trailzero __builtin_ctz
+
+int main() {
+	// your code goes here
+    FAST
+
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        
+    }
+    
+	return 0;
+}
